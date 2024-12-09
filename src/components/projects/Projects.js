e3c9960fd985c87e1bb6f14d126523b030c69686
RafayRasheed <royalsdigitizing@gmail.com>
2024-12-09 22:23:00 +0500

    dhdrhyr

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -16,27 +16,159 @@ const ProjectData = [
     title: "Flowers",
     des: "Flower embroidery is a delicate art that involves stitching floral designs onto fabric using various techniques and colorful threads. It enhances garments, accessories, and home décor with intricate, nature-inspired patterns.",
     images: [
-      "(1).BMP", "(2).BMP", "(3).BMP", "(4).BMP", "(6).BMP", "(7).BMP", "(8).BMP", 
-      "(9).BMP", "(10).BMP", "(11).BMP", "(12).BMP", "(13).BMP", "(14).BMP", "(15).BMP", "(16).BMP", 
-      "(17).BMP", "(18).BMP", "(19).BMP", "(20).BMP", "(21).BMP", "(22).BMP", "(23).BMP", "(24).BMP", 
-      "(25).BMP", "(1).jpeg", "(2).jpeg",
-      "(1).jpg", "(2).jpg", "(3).jpg", "(4).jpg", "(5).jpg", "(6).jpg", "(7).jpg", "(8).jpg", 
-      "(9).jpg", "(10).jpg", "(11).jpg", "(12).jpg"
-    ]
-
-
-  }, 
+      "(1).jpg",
+      "(2).jpg",
+      "(3).jpg",
+      "(4).jpg",
+      "(5).jpg",
+      "(6).jpg",
+      "(7).jpg",
+      "(8).jpg",
+      "(9).jpg",
+      "(10).jpg",
+      "(11).jpg",
+      "(12).jpg",
+      "(13).jpg",
+      "(14).jpg",
+      "(15).jpg",
+      "(16).jpg",
+      "(17).jpg",
+      "(18).jpg",
+      "(19).jpg",
+      "(20).jpg",
+      "(21).jpg",
+      "(22).jpg",
+      "(23).jpg",
+      "(24).jpg",
+      "(25).jpg",
+      "(26).jpg",
+      "(27).jpg",
+      "(28).jpg",
+      "(29).jpg",
+      "(30).jpg",
+      "(31).jpg",
+      "(32).jpg",
+      "(33).jpg",
+      "(34).jpg",
+      "(35).jpg",
+      "(36).jpg",
+      "(37).jpg",
+      "(38).jpg",
+    ],
+  },
   {
     title: "Collar and Hem",
     des: "Collar embroidery adds decorative designs to the neckline, enhancing garments with intricate patterns. Hem embroidery adorns the bottom edge of clothing, offering a stylish finish with unique, eye-catching details",
     images: [
-      "(1).BMP", "(2).BMP", "(3).BMP", "(4).BMP", "(5).BMP", "(6).BMP", "(7).BMP", "(8).BMP", "(9).BMP", "(10).BMP",
-      "(11).BMP", "(12).BMP", "(13).BMP", "(14).BMP", "(15).BMP", "(16).BMP", "(17).BMP", "(18).BMP", "(19).BMP", "(20).BMP",
-      "(21).BMP", "(22).BMP", "(23).BMP", "(24).BMP", "(25).BMP", "(26).BMP", "(27).BMP", "(28).BMP", "(29).BMP"
-    ]
+      "(1).jpg",
+      "(2).jpg",
+      "(3).jpg",
+      "(4).jpg",
+      "(5).jpg",
+      "(6).jpg",
+      "(7).jpg",
+      "(8).jpg",
+      "(9).jpg",
+      "(10).jpg",
+      "(11).jpg",
+      "(12).jpg",
+      "(13).jpg",
+      "(14).jpg",
+      "(15).jpg",
+      "(16).jpg",
+      "(17).jpg",
+      "(18).jpg",
+      "(19).jpg",
+      "(20).jpg",
+      "(21).jpg",
+      "(22).jpg",
+      "(23).jpg",
+      "(24).jpg",
+      "(25).jpg",
+      "(26).jpg",
+      "(27).jpg",
+      "(28).jpg",
+      "(29).jpg",
+    ],
+  },
 
-    
-  }, 
+  {
+    title: "Kids Designing",
+    des: "Collar embroidery adds decorative designs to the neckline, enhancing garments with intricate patterns. Hem embroidery adorns the bottom edge of clothing, offering a stylish finish with unique, eye-catching details",
+    images: [
+      "(1).jpg",
+      "(2).jpg",
+      "(3).jpg",
+      "(4).jpg",
+      "(5).jpg",
+      "(6).jpg",
+      "(7).jpg",
+      "(8).jpg",
+      "(9).jpg",
+      "(10).jpg",
+      "(11).jpg",
+      "(12).jpg",
+      "(13).jpg",
+      "(14).jpg",
+      "(15).jpg",
+      "(16).jpg",
+      "(17).jpg",
+      "(18).jpg",
+      "(19).jpg",
+      "(20).jpg",
+      "(21).jpg",
+      "(22).jpg",
+      "(23).jpg",
+      "(24).jpg",
+      "(25).jpg",
+      "(26).jpg",
+      "(27).jpg",
+      "(28).jpg",
+      "(29).jpg",
+      "(30).jpg",
+      "(31).jpg",
+      "(32).jpg",
+      "(33).jpg",
+      "(34).jpg",
+      "(35).jpg",
+      "(36).jpg",
+      "(37).jpg",
+      "(38).jpg",
+      "(39).jpg",
+      "(40).jpg",
+      "(41).jpg",
+      "(42).jpg",
+      "(43).jpg",
+      "(44).jpg",
+      "(45).jpg",
+      "(46).jpg",
+      "(47).jpg",
+      "(48).jpg",
+      "(49).jpg",
+      "(50).jpg",
+      "(51).jpg",
+      "(52).jpg",
+      "(53).jpg",
+      "(54).jpg",
+      "(55).jpg",
+      "(56).jpg",
+      "(57).jpg",
+      "(58).jpg",
+      "(59).jpg",
+      "(60).jpg",
+      "(61).jpg",
+      "(62).jpg",
+      "(63).jpg",
+      "(64).jpg",
+      "(65).jpg",
+      "(66).jpg",
+      "(67).jpg",
+      "(68).jpg",
+      "(69).jpg",
+      "(70).jpg",
+      "(71).jpg",
+    ],
+  },
 ];
 const Projects = () => {
   const [selectedItem, setSelectedItem] = useState(null);
@@ -94,8 +226,8 @@ const Projects = () => {
     <section id="projects" className="w-full py-10">
       <div className="flex justify-center items-center text-center">
         <Title
-         title="VISIT OUR WORK AND SHARE YOUR FEEDBACK"
-des="Our Creations"
+          title="VISIT OUR WORK AND SHARE YOUR FEEDBACK"
+          des="Our Creations"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6 xl:gap-14">
@@ -103,34 +235,34 @@ des="Our Creations"
           const { title, des, images } = item;
           return (
             <div
-            onClick={() => {
-              setSelectedItem(item);
-            }}
-            key={i}
-            className="w-full p-8 xl:px-12 h-auto xl:py-8 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000"
-          >
-            <ImageCarousel item={item}/>
-            {/* <div className="w-full h-[240px] overflow-hidden rounded-lg">
+              onClick={() => {
+                setSelectedItem(item);
+              }}
+              key={i}
+              className="w-full p-8 xl:px-12 h-auto xl:py-8 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000"
+            >
+              <ImageCarousel item={item} />
+              {/* <div className="w-full h-[240px] overflow-hidden rounded-lg">
               <img
                 className="w-full h-full object-cover group-hover:scale-110 duration-300 cursor-pointer"
                 src={require(`../../assets/categories/${title}/${images[10]}`)}
                 alt={title}
               />
             </div> */}
-            <div className="w-full mt-5 flex flex-col gap-6">
-              <div>
-                <div className="flex items-center justify-between">
-                  <h3 className="text-base uppercase text-designColor font-normal">
-                    {title}
-                  </h3>
-                  <ProjectLinks it={item} />
+              <div className="w-full mt-5 flex flex-col gap-6">
+                <div>
+                  <div className="flex items-center justify-between">
+                    <h3 className="text-base uppercase text-designColor font-normal">
+                      {title}
+                    </h3>
+                    <ProjectLinks it={item} />
+                  </div>
+                  <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+                    {des}
+                  </p>
                 </div>
-                <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
-                  {des}
-                </p>
               </div>
             </div>
-          </div>
           );
         })}
       </div>
